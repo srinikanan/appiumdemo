8f46053698cfcd224dbc5598db201e72cb4590f5
srinikanan <srini.kanan@gmail.com>
2020-02-03 21:59:32 +0530

    this my first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPpAndroid()
+	{
+		System.out.println("NativeAPpAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");	
+		}
+	
+	@Test
+	public void mobilechrome()
+	{
+		System.out.println("Mobilechome test");
+	}
+}
